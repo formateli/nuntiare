0070dbfcb46b6e5f4d51d51721cd8571e51f6336
formateli <formateli@gmail.com>
2020-01-15 09:56:22 -0500

    Highlight line by line wip

--- nuntiare/pluma/highlight/highlight.py
+++ nuntiare/pluma/highlight/highlight.py
@@ -79,23 +79,30 @@ class HighlightDefinition(XmlMixin):
             if n.nodeName == 'descriptors':
                 self._get_descriptors(n, case_sensitive)
 
-    def apply_hl(self, text, text_info):
+    def apply_hl(self, text, text_info, blocks):
         first_time = not text.tags_setted
         if first_time:
             text.set_tags(self._styles)
 
         if first_time:
-            self._apply_hl_first_time(text)
+            self._apply_hl_first_time(text, blocks)
         else:
-            self._apply_hl_text_changed(text, text_info)
+            self._apply_hl_text_changed(text, text_info, blocks)
 
-    def _apply_hl_first_time(self, text):
+    def _apply_hl_first_time(self, text, blocks):
         #TODO must be line by line
         for d in self._descriptors:
             d.apply_regex(text, text.index('1.0'), text.index('end'))
 
-    def _apply_hl_text_changed(self, text, text_info):
+    def _apply_hl_text_changed(self, text, text_info, blocks):
         pass
+
+    def _process_line(self, text, txt, line_no, blocks):
+        start_index = '{0}.{1}'.format(line_no, 0)
+        end_index = '{0}.{1}'.format(line_no, len(txt))
+        blks = []
+        for d in self._descriptors:
+            blks += d.get_blocks(text, start_index, end_index)
 
     def _get_styles(self, node):
         for n in node.childNodes:
@@ -144,6 +151,51 @@ class HighlightDescriptor(XmlMixin):
 
         self._pattern, self._pattern_1, self._pattern_2 = \
                     self._get_pattern()
+
+    def get_blocks(self, text, start, end, prefix=''):
+        if not self._pattern:
+            return
+
+        blocks = []
+
+        matchStart = prefix + 'matchStart'
+        matchEnd = prefix + 'matchEnd'
+        searchLimit = prefix + 'searchLimit'
+
+        text.mark_set(matchStart, start)
+        text.mark_set(matchEnd, start)
+        text.mark_set(searchLimit, end)
+
+        count = text.new_int_var()
+
+        while True:
+            index = text.search(self._pattern, matchEnd, searchLimit,
+                                count=count, regexp=True)
+            if index == '' or count.get() == 0:
+                if self._type in {'WholeWord', 'ToEOL'}:
+                    break
+                else:
+                    res_multiline = self._to_close_token_search(
+                            text, matchEnd, searchLimit, count, prefix)
+                    if not res_multiline:
+                        break
+                    self._apply_tag(text, res_multiline[0], res_multiline[1], prefix)
+            else:
+                end_index = text.index('{0}+{1}c'.format(index, count.get()))
+                bloks.append(HighlightBlock(
+                        index, 
+                        end_index,
+                        descriptor=self)
+                    )
+                text.mark_set(matchEnd, end_index)
+
+            #if self._descriptors: # Sub descriptors
+            #    for d in self._descriptors:
+            #        d.apply_regex(text,
+            #            text.index(matchStart), text.index(matchEnd + '-1c'),
+            #            prefix='s')
+
+        return blocks
 
     def apply_regex(self, text, start, end, prefix=''):
         if not self._pattern:
@@ -355,3 +407,24 @@ class HighlightToken(XmlMixin):
                 node, 'closeToken', None)
         self.escape_token = self.get_attr_value(
                 node, 'escapeToken', None)
+
+
+class HighlightBlocks():
+    def __init__(self):
+        self._blocks = []
+        self._lines = []
+
+
+class HighlightBlock():
+    def __init__(self, start_index, end_index, descriptor, state='COMPLETED'):
+        self._descriptor = descriptor
+        self.state = state # OPEN, COMPLETED
+
+        self._line_start, self._col_start = \
+            self._get_line_col(start_index)
+        self._line_end, self._col_end = \
+            self._get_line_col(end_index)
+
+    def _get_line_col(self, index):
+        s = index.split('.')
+        return s[0], s[1]
