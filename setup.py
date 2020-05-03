b0f5f5768d11930acc4b0849108dd269cd24e7d3
formateli <formateli@gmail.com>
2020-05-02 22:56:00 -0500

    setuptools

--- setup.py
+++ setup.py
@@ -4,41 +4,45 @@
 
 """Nuntiare setup script"""
 
-from distutils.core import setup
+import setuptools
 from nuntiare.version import get_version
 
 
-def run():
-    setup(
-        name="Nuntiare",
-        version=get_version(),
-        url="https://formateli.com/nuntiare/",
-        download_url="https://github.com/formateli/nuntiare",
-        description="Python report toolkit.",
-        author="Fredy Ramirez",
-        author_email="",  # TODO
-        maintainer_email="",  # TODO
-        classifiers=[
-            "Development Status :: Prototype",
-            "Environment :: Win32 (MS Windows)",
-            "Environment :: X11 Applications",
-            "Intended Audience :: Developers",
-            "Intended Audience :: Information Technology",
-            "License :: OSI Approved :: GNU General Public License v3",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-        ],
-        license="GNU General Public License v3",
-        platforms=["OS Independent"],
-        packages=[
-            "nuntiare", "nuntiare.data_providers", "nuntiare.outcome",
-            "nuntiare.template", "nuntiare.render", "nuntiare.render.html"
-        ],
-        package_data={'nuntiare': ['nuntiare.cfg']},
-        scripts=['scripts/nuntiare', 'scripts/pluma'],
-        data_files=[("docs", ["README", "LICENSE", "COPYRIGHT"])],
-    )
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
 
 
-if __name__ == "__main__":
-    run()
+setuptools.setup(
+    name='Nuntiare',
+    version=get_version(),
+    author='Fredy Ramirez <https://formateli.com>',
+    author_email='',
+    description='Python report toolkit',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://formateli.com/nuntiare',
+    download_url='https://github.com/formateli/nuntiare',
+    packages=setuptools.find_packages(),
+    keywords='python report toolkit database',
+    project_urls={
+        "Bug Tracker": 'https://github.com/formateli/nuntiare',
+        "Documentation": 'https://formateli.com/nuntiare/docs',
+        "Forum": '',
+        "Source Code": 'https://github.com/formateli/nuntiare',
+        },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Natural Language :: Spanish",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Database",
+        "Topic :: Office/Business",
+        "Topic :: Printing",
+    ],
+    install_requires=[],
+    python_requires='>=3.6',
+)
